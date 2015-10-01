df5329c9bc63863d649e3c08ae85f465122563ee
danielefenix <daniele.salvatore@fao.org>
2015-10-01 11:12:56 +0200

    update HTML files to HBS

--- src/js/templates/base_template.js
+++ src/js/templates/base_template.js
@@ -1,7 +1,7 @@
 /*global define*/
 define([
         'jquery',
-        'text!fx-m-c/html/templates/base_template.html'
+        'text!fx-m-c/html/templates/base_template.hbs'
     ],
     function ($, template) {
 
