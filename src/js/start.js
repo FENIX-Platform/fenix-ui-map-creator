7d333438b1133d3903fd860d31ccdfbf2038c6c7
danielefenix <daniele.salvatore@fao.org>
2015-12-16 17:17:20 +0100

    .

--- src/js/start.js
+++ src/js/start.js
@@ -1,11 +1,16 @@
 /*global define*/
-define([
-        'require',
+define(['require',
         'jquery',
+        'loglevel',
+        'q',
+       // 'fx-m-c/config/events',
         'fx-m-c/templates/base_template',
-        'fx-m-c/adapters/FENIX_fx_map'
+        'fx-m-c/adapters/FENIX_fx_map',
+        'amplify'
     ],
-    function (RequireJS, $) {
+    function (RequireJS, $, log, Q)
+             // E)
+    {
 
         'use strict';
 
@@ -19,22 +24,26 @@ define([
         }
 
         MapCreator.prototype.render = function (config) {
+            this.deferred = Q.defer();
 
-            var self = this;
             try {
                 if (this._validateInput(config)) {
                     this.preloadResources(config);
+
+                    this.bindEventListeners();
+
                 }
             }catch(e) {
-                self.onError(e);
+                this.onError(e);
             }
 
+            return this.deferred.promise;
         };
 
         MapCreator.prototype.preloadResources = function ( config ) {
 
             var baseTemplate = this.getTemplateUrl(),
-                adapter =  this.getAdapterUrl(),
+                adapter =  this.getAdapterUrl((config.adapter) ? config.adapter.adapterType : null),
                 self = this;
 
             RequireJS([
@@ -52,12 +61,22 @@ define([
                 if (typeof config.onReady === 'function') {
                     config.onReady(self);
                 }
+
+                self.deferred.resolve(self);
             });
         };
 
-        MapCreator.prototype.getAdapterUrl = function () {
+        MapCreator.prototype.getAdapterUrl = function (adapterType) {
             //TODO add here adapter discovery logic
-            return this.adapterUrl ? this.adapterUrl : 'fx-m-c/adapters/FENIX_fx_map';
+            if (adapterType !== null && adapterType !== undefined) {
+                switch (adapterType.toLocaleLowerCase()) {
+                    case 'fenix':
+                        return this.adapterUrl ? this.adapterUrl : 'fx-m-c/adapters/FENIX_fx_map';
+              }
+            }
+            else {
+                return this.adapterUrl ? this.adapterUrl : 'fx-m-c/adapters/FENIX_fx_map';
+            }
         };
 
         MapCreator.prototype.getTemplateUrl = function () {
@@ -69,9 +88,17 @@ define([
             return true;
         };
 
+        MapCreator.prototype.getContainer = function () {
+            return this.template.container;
+        };
+
+        MapCreator.prototype.addBaseLayer = function (layer) {
+            return this.adapter.addBaseLayer(layer);
+        };
+
         // Handle Layers
-        MapCreator.prototype.addLayer = function (model, options) {
-            return this.adapter.addLayer(model, options);
+        MapCreator.prototype.addLayer = function (model, layerOptions, modelOptions) {
+            return this.adapter.addLayer(model, layerOptions, modelOptions);
         };
 
         MapCreator.prototype.removeLayer = function (layer) {
@@ -82,12 +109,32 @@ define([
             return this.adapter.addCountryBoundaries();
         };
 
-        MapCreator.prototype.addCountryLabels = function () {
-            return this.adapter.addCountryLabels();
+        MapCreator.prototype.invalidateSize = function () {
+
+            log.info('invalidateSize', this.template.template.title)
+            // dirty fix for invalidate size
+            return this.adapter.invalidateSize();
+
         };
 
-        MapCreator.prototype.invalidateSize = function () {
-            return this.adapter.invalidateSize();
+        MapCreator.prototype.bindEventListeners = function () {
+
+            // amplify.subscribe(E.WINDOW_RESIZE, this, this.invalidateSize);
+
+        };
+
+        MapCreator.prototype.unbindEventListeners = function () {
+
+            // amplify.unsubscribe(E.WINDOW_RESIZE, this, this.invalidateSize);
+
+        };
+
+        MapCreator.prototype.destroy = function () {
+
+            //log.info('Map creator destroy ', this.template.template.title)
+
+            this.unbindEventListeners();
+
         };
 
         return MapCreator;
