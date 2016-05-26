d124611b473950e3cfa993ab92709363979facfb
stefanocudini <stefano.cudini@gmail.com>
2016-05-26 10:58:35 +0200

    keys

--- test/js/test.js
+++ test/js/test.js
@@ -47,20 +47,20 @@ define([
                     wmsLoader: false
                 },
                 baselayers: {
-                    "cartodb": {
+                    cartodb: {
                         title_en: "CartoDB light",
                         url: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
                         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
                         subdomains: 'abcd',
                         maxZoom: 19
                     },
-                    "esri_grayscale": {
+                    esri_grayscale: {
                         url: "http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
                         title_en: "Esri WorldGrayCanvas",
                         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
                         maxZoom: 16
                     }
-                }                
+                }
             }
         });
 
